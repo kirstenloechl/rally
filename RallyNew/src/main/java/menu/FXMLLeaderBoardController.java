dd287b6ca1c2aa420feb0a237e4f2d7d450662f1
Daniel Kirkpatrick <thedanieljk@gmail.com>
2018-06-06 14:26:36 -0700

    Added leaderboard

--- RallyNew/src/main/java/menu/FXMLLeaderBoardController.java
+++ RallyNew/src/main/java/menu/FXMLLeaderBoardController.java
@@ -1,37 +1,85 @@
-package menu;
+package main_menu;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.stage.Screen;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.StageStyle;
+import javafx.geometry.*;
+import javafx.scene.text.TextAlignment;
 
 public class FXMLLeaderBoardController {
 
-	static final int MENU_WIDTH = 1024;
-	static final int MENU_HEIGHT = 664;
+	static final int WINDOW_HEIGHT = 664, WINDOW_WIDTH = 1024;
 	
 	@FXML
 	Button btnReturn;
 	
 	@FXML
-	protected void handleReturn(ActionEvent event) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXMLMainMenu.fxml"));
-		Scene main = new Scene(root, MENU_WIDTH, MENU_HEIGHT); 	
+	protected void handleReturn(ActionEvent event) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("FXMLMainMenu.fxml"));
+		Scene main = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT); 	
 		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		
-		Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
-	    stage.setX((screenBounds.getWidth() - MENU_WIDTH) / 2); 
-		stage.setY((screenBounds.getHeight() - MENU_HEIGHT) / 2);  
-		
 		stage.setScene(main);
-		stage.setTitle("Rally - Leaderboard");
 		stage.show();
+
+	}
+	
+	@FXML
+	protected void getLeaderboard(Stage stage) {
+		// pane = FXMLLoader.load(getClass().getResource("FXMLLeaderboard.fxml"));
+	    Connection c = null;
+	    Statement stmt = null;
+	    int i = 1;
+	    int x = 10;
+	    int y = 10;
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:database.db");
+	      c.setAutoCommit(false);      
+	      stmt = c.createStatement();
+	      ResultSet rs = stmt.executeQuery( "SELECT * FROM logins ORDER by highscore DESC LIMIT 5");
+	      Pane pane = new Pane();
+	      Pane newPane = FXMLLoader.load(getClass().getResource("FXMLLeaderboard.fxml"));
+	      pane.getChildren().add(newPane);
+	      while (rs.next()) {
+	    	  Text t = new Text();
+	    	  String user = rs.getString("username");
+	    	  String score = Integer.toString(rs.getInt("highscore"));
+	    	  String done = String.format("%-33s%d%-3s%-20s%4s\n"," ",i++,".",user,score);
+	    	  t.setText(done);
+	    	  t.setY(y+=100);
+	    	  t.setStyle("-fx-font: 36 arial;");
+	    	  pane.getChildren().add(t);
+
+	      }
+	      rs.close();
+	      stmt.close();
+	      c.close();
+	      Scene scene2 = new Scene(pane,WINDOW_WIDTH,WINDOW_HEIGHT);
+	      stage.setScene(scene2);
+    	  stage.show();
+		}
+		catch (Exception e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);  
+		}
 	}
 }
