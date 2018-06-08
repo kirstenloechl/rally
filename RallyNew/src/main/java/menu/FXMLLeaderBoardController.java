dd035574c28019e466b4ee68e8cc2da206f6ca6e
Pat-Farrell-17 <38262281+Pat-Farrell-17@users.noreply.github.com>
2018-06-07 19:40:35 -0700

    Fixed some code smells

--- RallyNew/src/main/java/menu/FXMLLeaderBoardController.java
+++ RallyNew/src/main/java/menu/FXMLLeaderBoardController.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
-import java.util.ArrayList;
 import java.sql.*;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -13,7 +12,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 import javafx.scene.layout.Pane;
@@ -25,15 +23,13 @@ public class FXMLLeaderBoardController {
 	
 	@FXML
 	Button btnReturn;
-	@FXML
-	Label user_1, user_2, user_3, user_4, user_5, score_1, score_2, score_3, score_4, score_5;
 	
 	@FXML
 	protected void handleReturn(ActionEvent event) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXMLMainMenu.fxml"));
 		Scene main = new Scene(root, WINDOW_WIDTH, WINDOW_HEIGHT); 	
 		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-		
+		stage.setTitle("Rally - Menu");
 		stage.setScene(main);
 		stage.show();
 
@@ -42,7 +38,6 @@ public class FXMLLeaderBoardController {
 	@FXML
 	protected void getLeaderboard(Stage stage) {
 
-	    int i = 1;
 	    int y = 150;
 	    int x1 = 300;
 	    int x2 = 650;
